3f121218a2dfd51192331a7302fd56e70da2991d
npgiano <nick@stone.money>
2026-05-28 14:37:40 -0400

    Progress bar

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, May 28, 2026 @ 11:36:13 ET
+ *  Date: Thursday, May 28, 2026 @ 14:37:29 ET
  *  By: nick
  *  ENGrid styles: v0.25.4
  *  ENGrid scripts: v0.25.2
@@ -25430,6 +25430,37 @@ const customScript = function (App) {
   console.log("ENGrid client scripts are executing");
   // Add your client scripts here
   App.setBodyData("client-js-loading", "finished");
+
+  // ** PROGRESS BAR COUNT POSITIONING **
+  const progressBar = document.querySelector(".en__component--widgetblock");
+  if (progressBar) {
+    // Progress bar takes a moment to load, so we need to wait for it to be fully rendered before we can move the count element
+    if (!progressBar.querySelector(".enWidget__fill__count")) {} else {
+      const count = progressBar.querySelector(".enWidget__fill__count");
+      if (count) {
+        const progress = progressBar.querySelector(".enWidget__progress");
+        progress?.appendChild(count);
+      }
+      return;
+    }
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.type === "childList") {
+          const count = progressBar.querySelector(".enWidget__fill__count");
+          if (count) {
+            const progress = progressBar.querySelector(".enWidget__progress");
+            progress?.appendChild(count);
+            observer.disconnect();
+          }
+        }
+      });
+    });
+    observer.observe(progressBar, {
+      childList: true,
+      subtree: true
+    });
+  }
+  // ** END PROGRESS BAR COUNT POSITIONING **
 };
 // EXTERNAL MODULE: ./node_modules/smoothscroll-polyfill/dist/smoothscroll.js
 var smoothscroll = __webpack_require__(1196);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,33 @@ export const customScript = function (App) {
   console.log("ENGrid client scripts are executing");
   // Add your client scripts here
   App.setBodyData("client-js-loading", "finished");
+
+  // ** PROGRESS BAR COUNT POSITIONING **
+  const progressBar = document.querySelector(".en__component--widgetblock");
+  if (progressBar) {
+    // Progress bar takes a moment to load, so we need to wait for it to be fully rendered before we can move the count element
+    if (!progressBar.querySelector(".enWidget__fill__count")) {
+    } else {
+      const count = progressBar.querySelector(".enWidget__fill__count");
+      if (count) {
+        const progress = progressBar.querySelector(".enWidget__progress");
+        progress?.appendChild(count);
+      }
+      return;
+    }
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.type === "childList") {
+          const count = progressBar.querySelector(".enWidget__fill__count");
+          if (count) {
+            const progress = progressBar.querySelector(".enWidget__progress");
+            progress?.appendChild(count);
+            observer.disconnect();
+          }
+        }
+      });
+    });
+    observer.observe(progressBar, { childList: true, subtree: true });
+  }
+  // ** END PROGRESS BAR COUNT POSITIONING **
 };
