b78ba1c9b21d64601ee5d80ddcb68cde88738caf
npgiano <nick@stone.money>
2026-06-02 18:59:59 -0400

    feat: Matching mockups closely

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, June 2, 2026 @ 15:24:45 ET
+ *  Date: Tuesday, June 2, 2026 @ 18:59:30 ET
  *  By: nick
  *  ENGrid styles: v0.25.4
  *  ENGrid scripts: v0.25.2
